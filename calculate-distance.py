2ed94d6865e4529435d2f6dff9176de6dc5f1a00
Shafin M <shafinmohammed071@gmail.com>
2020-08-13 15:48:14 -0700

    added comments

--- calculate-distance.py
+++ calculate-distance.py
@@ -1,7 +1,14 @@
 
 def calculate_distance():
+    """
+    Function prompts user to enter coordinates for the two points (x1, y1, x2, y2)
+    Function prompts user to enter distance type. E for Euclidean or M for Manhattan
+    The distance is then printed out
+    """
     print('Welcome to the coordinate distance Calculator!')
     print('Please enter the x & y coordinates of your two data points, followed by the distance type')
+
+    # Prompt user to provide data points coordinate
     while True:
         try:
             x1 = float(input("x1: "))
@@ -14,6 +21,7 @@ def calculate_distance():
 
     dist_type_dict = {'E': 'Euclidean', 'M': 'Manhattan'}
 
+    # Prompt user to provide distance type
     while True:
         dist_type = input("Distance Type (enter E for Euclidean or M for Manhattan): ")
         if dist_type in dist_type_dict:
@@ -21,7 +29,7 @@ def calculate_distance():
         else:
             print('Incorrect input. Please enter enter E for Euclidean or M for Manhattan')
 
-
+    # Calculate distance and print result
     if dist_type is 'E':
         dist = ( (x1 - x2)**2 + (y1 - y2)**2)**0.5
     elif dist_type is 'M':
